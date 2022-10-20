fc735522fcc6b1bc91ee335fe597b9a1cbd5c68b
anagalit <ana.galit1@gmail.com>
2022-10-20 09:37:11 +0200

    Added adjustments in Character.css

--- src/CharacterCard.tsx
+++ src/CharacterCard.tsx
@@ -1,15 +1,16 @@
 import React from "react";
 import {CharacterModel} from "./CharacterModel";
 
-type characterProps = {characterProps:CharacterModel};
+type characterProps = { characterProps: CharacterModel };
 
 function CharacterCard(props: characterProps) {
     return <>
         <div className="CharacterCard">
-           <h3>{props.characterProps.name}</h3>
-           <img src={props.characterProps.image}/>
-           <p className={origin}>{props.characterProps.origin.name}</p>
+            <h3>{props.characterProps.name}</h3>
+            <img src={props.characterProps.image}/>
+            <h4>{props.characterProps.origin.name}</h4>
         </div>
     </>
 }
+
 export default CharacterCard;
--- src/CharacterGallery.tsx
+++ src/CharacterGallery.tsx
@@ -1,16 +1,15 @@
 import React from 'react';
 import CharacterCard from "./CharacterCard";
-import characterCard from "./CharacterCard";
 import {CharacterModel} from "./CharacterModel";
 
-function CharacterGallery(props: {gallery:CharacterModel[]}) {
- return (
-     <>
-         {
-             props.gallery.map(character => <CharacterCard characterProps={character} key={character.id}/>)
-         }
-         </>
- )
+function CharacterGallery(props: { gallery: CharacterModel[] }) {
+    return (
+        <>
+            {
+                props.gallery.map(character => <CharacterCard characterProps={character} key={character.id}/>)
+            }
+        </>
+    )
 }
 
 export default CharacterGallery;
